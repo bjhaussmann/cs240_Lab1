3bc1915a1f4ddcaaac3c2d125d78df004bc41c8f
bjhaussmann <bjhaussmann@gmail.com>
2017-12-05 15:11:17 -0800

    done

--- src/cs240_Lab1/QuickSort.java
+++ src/cs240_Lab1/QuickSort.java
@@ -21,7 +21,35 @@ public class QuickSort {
 			return array;
 		}
 		return array;
-		
+
+	}
+
+	static int countMove = 0;
+	static int countCompare = 0;
+
+	int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = (low - 1);
+		for (int j = low; j < high; j++) {
+
+			if (arr[j] <= pivot) {
+				i++;
+
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				++countCompare;
+				++countMove;
+			}
+		}
+
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+		++countMove;
+		++countCompare;
+
+		return i + 1;
 	}
 
 	/**
@@ -40,7 +68,7 @@ public class QuickSort {
 
 	/**
 	 * Recursively implements quick sort with a separate index parameters to start
-	 *  and end sorting at any part of the array.
+	 * and end sorting at any part of the array.
 	 * 
 	 * @param array
 	 *            Array to be sorted.
@@ -56,9 +84,12 @@ public class QuickSort {
 		}
 
 		if (low < high) {
+			int pi = partition(array, low, high);
+			++countCompare;
 
+			recursiveSort(array, low, pi - 1);
+			recursiveSort(array, pi + 1, high);
 		}
-		recursiveSort(array, low, high);
 		return array;
 	}
 }
--- src/cs240_Lab1/RadixSort.java
+++ src/cs240_Lab1/RadixSort.java
@@ -16,27 +16,66 @@ public class RadixSort {
 	 *            Array to be sorted
 	 * @return array Sorted array.
 	 */
-	public int[] iterativeSort(int[] array) {
+	public int[] iterativeSort(int[] array, int n) {
 		if (array == null) {
 			return array;
+		}
+		int m = getMax(array, n);
+
+		for (int exp = 1; m/exp > 0; exp *= 10){
+			countSort(array, n, exp);
+			++countCompare;
 		}
 		return array;
 		
 	}
 
-	/**
-	 * Recursively implements radix sort on the entire array.
-	 * 
-	 * @param array
-	 *            Array to be sorted.
-	 * @return array Sorted array.
-	 */
-	public int[] recursiveSort(int[] array) {
-		if (array == null) {
-			return array;
+	static int countMove = 0;
+    static int countCompare = 0;
+    
+	
+	static int getMax(int arr[], int n)
+	{
+		int mx = arr[0];
+		for (int i = 1; i < n; i++){
+		    ++countCompare;
+			if (arr[i] > mx){
+				mx = arr[i];
 		}
-		return recursiveSort(array, 0);
 	}
+		return mx;
+	}
+
+	static void countSort(int arr[], int n, int exp)
+	{
+		int output[] = new int[n]; 
+		int i;
+		int count[] = new int[10];
+
+		for (i = 0; i < n; i++){
+			count[ (arr[i]/exp)%10 ]++;
+			++countMove;
+			++countCompare;
+	}
+
+		for (i = 1; i < 10; i++){
+			count[i] += count[i - 1];
+			++countCompare;
+			++countMove;
+		}
+
+		for (i = n - 1; i >= 0; i--)
+		{
+			output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+			count[ (arr[i]/exp)%10 ]--;
+			++countMove;
+			++countCompare;
+		}
+
+		for (i = 0; i < n; i++)
+			arr[i] = output[i];
+	}
+	
 
 	/**
 	 * Recursively implements radix sort with a separate index parameter to start
@@ -46,11 +85,25 @@ public class RadixSort {
 	 *            Array to be sorted.
 	 * @return array Sorted array.
 	 */
-	public int[] recursiveSort(int[] array, int index) {
-		if (array == null) {
-			return array;
-		}
-		recursiveSort(array, index++);
-		return array;
+	public void recursiveSort(int[] array1, int[] array2, int exp, int m, int index) {
+		 while(exp > 0)
+ 		{
+ 			if (m / exp < 0)
+ 				return;
+ 			else
+ 			{
+             
+                 for (int i = 0; i < index; i++)
+                     array2[(array2[i] / exp) % 10]++;
+                 for (int j = 1; j < 10; j++)
+                     array2[j] += array2[j - 1];
+                 for (int k = index - 1; k >= 0; k--)
+                     array1[--array2[(array2[k] / exp) % 10]] = array2[k];
+                 for (int a = 0; a < index; a++)
+                     array2[a] = array1[a];
+                 exp *= 10; 
+                 recursiveSort(array2, array1,exp, m, index);
+ 			}
+ 		}
 	}
 }
